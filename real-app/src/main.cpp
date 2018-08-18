6d2a9d7d6602b33521f21bff6990db88c0048829
mini)(ant <20887697@users.noreply.github.com>
2018-08-18 12:40:43 +0300

    Fix errors not being reported.

--- real-app/src/main.cpp
+++ real-app/src/main.cpp
@@ -21,11 +21,18 @@ int main(int argc, char** argv) {
         << "Project repository: https://github.com/miniant-git/REAL" << std::endl << std::endl;
 
     int errorCode = MinimumLatencyAudioClient().Start();
-    std::cout << "Minimum audio latency enabled on the DEFAULT playback device!" << std::endl << std::endl;
+    if (errorCode == 0)
+        std::cout << "Minimum audio latency enabled on the DEFAULT playback device!" << std::endl << std::endl;
+    else
+        std::cout << "ERROR: Could not enable low-latency mode. Error code " << errorCode << "." << std::endl << std::endl;
 
     std::cout << "Checking for updates..." << std::endl;
     AutoUpdater(APP_VERSION, argv[0]).Update();
 
-    WaitForAnyKey("\nPress any key to disable and quit . . .");
+    if (errorCode == 0) 
+        WaitForAnyKey("\nPress any key to disable and exit . . .");
+    else
+        WaitForAnyKey("\nPress any key to exit . . .");
+
     return errorCode;
 }
