bf32d8f25d5fa09633b1dcb251699bc73bcc153c
mini)(ant <dev.miniant@gmail.com>
2018-09-02 16:56:24 +0300

    Refactor AutoUpdater class to remove duplicate logic.

--- real-app/src/AutoUpdater.cpp
+++ real-app/src/AutoUpdater.cpp
@@ -21,6 +21,52 @@ using namespace miniant::WindowsFilesystem;
 
 using json = nlohmann::json;
 
+std::optional<json> GetUpdaterRelease(CurlHandle& curl) {
+    curl.Reset();
+    curl.SetUrl("https://api.github.com/repos/miniant-git/REAL/releases/tags/updater-v2");
+    curl.SetUserAgent("real_updater_v1");
+
+    CurlMemoryWriter memoryWriter;
+    long responseCode = memoryWriter.InitiateRequest(curl);
+    if (responseCode != 200) {
+        return {};
+    }
+
+    json response = json::parse(memoryWriter.GetBuffer());
+    std::optional<Version> version = Version::Find(response["name"]);
+    if (!version) {
+        return {};
+    }
+
+    return response;
+}
+
+std::optional<json> GetUpdateRelease(const Version& currentVersion, CurlHandle& curl) {
+    std::optional<json> updaterRelease = GetUpdaterRelease(curl);
+    if (updaterRelease)
+        return updaterRelease;
+
+    curl.Reset();
+    curl.SetUrl("https://api.github.com/repos/miniant-git/REAL/releases/latest");
+    curl.SetUserAgent("real_updater_v1");
+
+    CurlMemoryWriter memoryWriter;
+    long responseCode = memoryWriter.InitiateRequest(curl);
+    if (responseCode != 200) {
+        std::cout << "Check for updates failed." << std::endl;
+        return {};
+    }
+
+    json response = json::parse(memoryWriter.GetBuffer());
+    std::optional<Version> latestVersion = Version::Parse(response["tag_name"]);
+    if (!latestVersion || currentVersion >= latestVersion.value()) {
+        std::cout << "The application is up-to-date." << std::endl;
+        return {};
+    }
+
+    return response;
+}
+
 std::optional<const json*> FindExecutableAsset(const json& response) {
     for (const auto& asset : response["assets"]) {
         if (asset["name"] == "update")
@@ -64,29 +110,14 @@ AutoUpdater::~AutoUpdater() {
 }
 
 bool AutoUpdater::Update() const {
-    if (this->UpdateUpdater())
+    CurlHandle curl;
+    std::optional<json> response = GetUpdateRelease(m_currentVersion, curl);
+    if (!response)
         return true;
 
-    CurlHandle curl;
-    curl.SetUrl("https://api.github.com/repos/miniant-git/REAL/releases/latest");
-    curl.SetUserAgent("real_updater_v1");
-
-    CurlMemoryWriter memoryWriter;
-    long responseCode = memoryWriter.InitiateRequest(curl);
-    if (responseCode != 200) {
-        std::cout << "Check for updates failed." << std::endl;
-        return false;
-    }
-
-    json response = json::parse(memoryWriter.GetBuffer());
-    std::optional<Version> latestVersion = Version::Parse(response["tag_name"]);
-    if (!latestVersion || m_currentVersion >= latestVersion.value()) {
-        std::cout << "The application is up-to-date." << std::endl;
-        return false;
-    }
-
     std::cout << "A new update is available!" << std::endl;
-    DisplayReleaseNotes(response["body"]);
+    DisplayReleaseNotes(response.value()["body"]);
+    std::optional<Version> latestVersion = Version::Parse(response.value()["tag_name"]);
     std::cout << "Do you want to update to " << latestVersion.value().ToString() << "? [y/N]: ";
     char line[5];
     std::cin.getline(line, 5);
@@ -97,7 +128,7 @@ bool AutoUpdater::Update() const {
         return false;
     }
 
-    std::optional<const json*> executableAsset = FindExecutableAsset(response);
+    std::optional<const json*> executableAsset = FindExecutableAsset(response.value());
     if (!executableAsset) {
         std::cout << "Error: misconfigured update assets." << std::endl;
         return false;
@@ -108,74 +139,6 @@ bool AutoUpdater::Update() const {
         std::cout << "Error: Could not create temporary app directory: "
             << std::filesystem::path(tempDirectory) << std::endl;
         return false;
-    }
-
-    std::filesystem::path tempExecutable(tempDirectory + TEXT("update"));
-    const char* url = static_cast<std::string>((*executableAsset.value())["browser_download_url"]).c_str();
-    curl.Reset();
-    curl.SetUrl(url);
-    curl.FollowRedirects(true);
-
-    CurlFileWriter fileWriter(tempExecutable);
-    std::cout << "Downloading update..." << std::endl;
-    fileWriter.InitiateRequest(curl);
-    fileWriter.Close();
-
-    WindowsString executable = GetExecutablePath();
-    std::optional<WindowsString> executableDirectory = GetParentDirectory(executable);
-    WindowsString renameCommand = GetRenameCommand(executable, TEXT("REAL.exe~DELETE"));
-    WindowsString moveCommand = GetMoveCommand(tempExecutable, executable);
-    if (!ExecuteCommand(
-        renameCommand + TEXT(" & ") + moveCommand, 
-        !CanWriteTo(executable) || !executableDirectory || !CanWriteTo(executableDirectory.value()))) {
-        std::cout << "A filesystem error was encountered during the update procedure." << std::endl;
-        return false;
-    }
-
-    std::cout << "Updated successfully! Restart the application to apply changes." << std::endl;
-    return true;
-}
-
-bool AutoUpdater::UpdateUpdater() const {
-    CurlHandle curl;
-    curl.SetUrl("https://api.github.com/repos/miniant-git/REAL/releases/tags/updater-v2");
-    curl.SetUserAgent("real_updater_v1");
-    
-    CurlMemoryWriter memoryWriter;
-    long responseCode = memoryWriter.InitiateRequest(curl);
-    if (responseCode != 200) {
-        return false;
-    }
-
-    json response = json::parse(memoryWriter.GetBuffer());
-    std::optional<Version> version = Version::Find(response["name"]);
-    if (!version) {
-        return false;
-    }
-
-    std::cout << "A new update is available!" << std::endl;
-    DisplayReleaseNotes(response["body"]);
-    std::cout << "Do you want to update to " << version.value().ToString() << "? [y/N]: ";
-    char line[5];
-    std::cin.getline(line, 5);
-    std::string prompt(line);
-    std::transform(prompt.begin(), prompt.end(), prompt.begin(), std::tolower);
-    if (prompt != "y" && prompt != "yes") {
-        std::cout << "No: Keeping the current version." << std::endl;
-        return true;
-    }
-
-    std::optional<const json*> executableAsset = FindExecutableAsset(response);
-    if (!executableAsset) {
-        std::cout << "Error: misconfigured update assets." << std::endl;
-        return true;
-    }
-
-    WindowsString tempDirectory = GetAppTempDirectory();
-    if (!CreateDirectory(tempDirectory)) {
-        std::cout << "Error: Could not create temporary app directory: "
-            << std::filesystem::path(tempDirectory) << std::endl;
-        return true;
     }
 
     std::filesystem::path tempExecutable(tempDirectory + TEXT("update"));
@@ -197,7 +160,7 @@ bool AutoUpdater::UpdateUpdater() const {
         renameCommand + TEXT(" & ") + moveCommand,
         !CanWriteTo(executable) || !executableDirectory || !CanWriteTo(executableDirectory.value()))) {
         std::cout << "A filesystem error was encountered during the update procedure." << std::endl;
-        return true;
+        return false;
     }
 
     std::cout << "Updated successfully! Restart the application to apply changes." << std::endl;
