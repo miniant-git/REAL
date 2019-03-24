e862057a6c3120e2ad523ee6a8f36512c9ec4fe0
mini)(ant <dev.miniant@gmail.com>
2019-03-24 14:01:58 +0200

    Print default audio device's properties. Addresses #2.

--- real-app/src/Windows/MinimumLatencyAudioClient.h
+++ real-app/src/Windows/MinimumLatencyAudioClient.h
@@ -1,10 +1,39 @@
 #pragma once
 
+#include "WindowsError.h"
+
+#include <tl/expected.hpp>
+
 namespace miniant::Windows::WasapiLatency {
 
 class MinimumLatencyAudioClient {
 public:
-    int Start() const;
+    struct Properties {
+        uint32_t defaultBufferSize;
+        uint32_t fundamentalBufferSize;
+        uint32_t minimumBufferSize;
+        uint32_t maximumBufferSize;
+        uint32_t sampleRate;
+        uint16_t bitsPerSample;
+        uint16_t numChannels;
+    };
+
+    MinimumLatencyAudioClient(MinimumLatencyAudioClient&& other);
+    ~MinimumLatencyAudioClient();
+
+    MinimumLatencyAudioClient& operator= (MinimumLatencyAudioClient&& rhs);
+
+    tl::expected<Properties, WindowsError> GetProperties();
+
+    static tl::expected<MinimumLatencyAudioClient, WindowsError> Start();
+
+private:
+    void* m_pAudioClient;
+    void* m_pFormat;
+
+    MinimumLatencyAudioClient(void* pAudioClient, void* pFormat);
+
+    void Uninitialise();
 };
 
 }
